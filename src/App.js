02187744794b1242865e4323ac62afda59a60458
Yaro Shiroki <yaro.shiroki@gmail.com>
2021-05-03 19:37:13 +0100

    Revert "firestore and messages start"
    This reverts commit f57c7cf1643c71028d2a0da466f2c5050d4cfb51.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Application from "./components/Application";
-import { auth } from "./firebase";
 import UserProvider from "./providers/UserProvider";
 
-const db = firebase.firestore();
-
 function App() {
-  const [user, setUser] = useState(() => auth.currentUser);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-      if (initializing) {
-        setInitializing(false);
-      }
-    })
-    //clean up subscription
-    return unsubscribe;
-  }, []);
-
-  if (initializing) return "Loading...";
+  
   return (
     <UserProvider>
       <Application />
--- src/components/SecondaryComponents/Messages.jsx
+++ src/components/SecondaryComponents/Messages.jsx
@@ -1,32 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
-import firebase from 'firebase/app';
 
-const MessageChannel = ({user=null, db = null}) => {
-    const[messages, setMessages] = useState([]);
+export default function About() {
+    return (
+        
+        <Typography variant="h1" component="h2" gutterBottom>
+            Messages Page
+        </Typography>   
 
-    useEffect((db) => {
-        if(db) {
-            const unsubscribe = db
-            .collection('messages')
-            .orderBy('createdAT')
-            .limit(100)
-            .onSnapshot(querySnapshot=>{
-                //get all document data from collection with IDs
-                const data = querySnapshot.docs.map(doc => ({
-                    ...doc.data(),
-                    id: doc.id,
-                }));
-                setMessages(data);
-            })
-
-            //detach listener
-            return unsubscribe;
-        }
-    }, [db]);
-
-    return null;
-}
+    );
+  }
   
-  export default MessageChannel;
+  export { default as Messages } from "./Messages";
