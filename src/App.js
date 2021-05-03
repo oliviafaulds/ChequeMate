d2b4cfa910b2907cb7bf93de810941fc16f64d84
Yaro Shiroki <yaro.shiroki@gmail.com>
2021-05-03 20:11:09 +0100

    Revert "Component file fix"
    This reverts commit 4233a2073454bb261ba3c1862aad54ba04facaa3.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Application from "./components/Application";
+import Application from "./Components/Application";
 import UserProvider from "./providers/UserProvider";
 
 function App() {
