f57c7cf1643c71028d2a0da466f2c5050d4cfb51
Yaro Shiroki <yaro.shiroki@gmail.com>
2021-05-03 19:13:30 +0100

    firestore and messages start

--- src/components/SecondaryComponents/Messages.jsx
+++ src/components/SecondaryComponents/Messages.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, withRouter } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
+import firebase from 'firebase/app';
 
-export default function About() {
-    return (
-        
-        <Typography variant="h1" component="h2" gutterBottom>
-            Messages Page
-        </Typography>   
+const MessageChannel = ({user=null, db = null}) => {
+    const[messages, setMessages] = useState([]);
 
-    );
-  }
+    useEffect((db) => {
+        if(db) {
+            const unsubscribe = db
+            .collection('messages')
+            .orderBy('createdAT')
+            .limit(100)
+            .onSnapshot(querySnapshot=>{
+                //get all document data from collection with IDs
+                const data = querySnapshot.docs.map(doc => ({
+                    ...doc.data(),
+                    id: doc.id,
+                }));
+                setMessages(data);
+            })
+
+            //detach listener
+            return unsubscribe;
+        }
+    }, [db]);
+
+    return null;
+}
   
-  export { default as Messages } from "./Messages";
+  export default MessageChannel;
