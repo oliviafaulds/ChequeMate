304cfed5a2bf42440e5cbd9bb1267bba598d40bc
Emily <56634795+emilynagle@users.noreply.github.com>
2021-05-03 15:32:38 +0100

    Update Calendar.jsx
    calendar object

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+//import interactionPlugin from '@fullcalendar/interaction'
 
-export default function About() {
+/*export default function About() {
     return (
         
         <Typography variant="h1" component="h2" gutterBottom>
@@ -11,5 +14,35 @@ export default function About() {
 
     );
   }
-  
   export { default as Calendar } from "./Calendar";
+  */
+
+
+export  default class DemoApp extends React.Component {
+    state = {
+        weekendsVisible: true
+    }
+  render() {
+    return (
+      <FullCalendar
+        plugins={[ dayGridPlugin]}
+        headerToolbar={{
+            left: 'today',
+            center: 'title',
+            right: 'prev,next'
+          }}
+        eventContent={renderEventContent}
+        selectable={true}
+       
+      />
+    )
+  }
+}
+function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
+  }
