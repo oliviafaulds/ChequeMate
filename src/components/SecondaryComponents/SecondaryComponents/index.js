ce73b3f9874db5d83f678952c8683a494fcb218b
JayClaw101 <johanna@cehs.net>
2021-05-03 18:16:25 +0100

    Login Working

---
+++ src/components/SecondaryComponents/SecondaryComponents/index.js
@@ -0,0 +1,8 @@
+export {default as Dashboard} from './Dashboard';
+export {default as Spending} from './Spending';
+export {default as Transactions} from './Transactions';
+export {default as Calendar} from './Calendar';
+export {default as Households} from './Households';
+export {default as Messages} from './Messages';
+export {default as Settings} from './Settings';
+export {default as ProfilePage} from './ProfilePage';
