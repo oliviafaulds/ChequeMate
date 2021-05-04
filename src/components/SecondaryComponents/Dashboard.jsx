1279aa1f85b06f43c3148fc80a831b8293937d3f
hayleyesthertaylor <83606121+hayleyesthertaylor@users.noreply.github.com>
2021-05-04 18:35:24 +0100

    Update Dashboard.jsx

--- src/components/SecondaryComponents/Dashboard.jsx
+++ src/components/SecondaryComponents/Dashboard.jsx
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 //import Button from '@material-ui/core/Button';
 import Badge from '@material-ui/core/Badge';
 import MailIcon from '@material-ui/icons/Mail';
+import { PieChart } from 'react-minimal-pie-chart';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,7 +37,13 @@ export default function Dashboard() {
           </Typography>
         </Grid>
         <Grid item xs={6}>
-          
+          <Typography
+            align="centre"
+            variant="h4"
+            component="h2"
+            gutterbottom>
+            Spending breakdown:
+          </Typography>
         </Grid>
         <Grid item xs={6}>
           <Typography
@@ -48,7 +55,13 @@ export default function Dashboard() {
           </Typography>
         </Grid>
         <Grid item xs={6}>
-
+          <PieChart
+            data={[
+              { title: 'One', value: 10, color: '#5A86D5' }, //all these values can be changed once we link to database
+              { title: 'Two', value: 15, color: '#283C5F' },
+              { title: 'Three', value: 20, color: '#141E2F' },
+            ]}
+          />;
         </Grid>
         <Grid item xs={0.75}>
           <Badge 
