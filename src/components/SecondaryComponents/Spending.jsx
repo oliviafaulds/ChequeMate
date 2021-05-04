e16daf053411bf1923445d043f51574fb7ea287b
hayleyesthertaylor <83606121+hayleyesthertaylor@users.noreply.github.com>
2021-05-04 16:21:49 +0100

    Update Spending.jsx

--- src/components/SecondaryComponents/Spending.jsx
+++ src/components/SecondaryComponents/Spending.jsx
@@ -5,47 +5,65 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-//export { default as Spending } from "./Spending";
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
 }));
 
-export default function CenteredGrid() {
+export default function Spending() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+            <Typography 
+                align="center"
+                variant="h2"
+                component="h2"
+                gutterBottom>
+                Spending Overview
+            </Typography>     
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+                <Typography
+                align="right"
+                variant="h4"
+                component="h2"
+                gutterBottom>
+                Balance:
+            </Typography>
         </Grid>
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+            <Typography
+                align="left"
+                variant="h4"
+                component="h2"
+                gutterBottom>
+                *£X.00*
+            </Typography>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+            <Typography
+                align="left"
+                variant="subtitle1"
+                component="h2"
+                gutterBottom>
+                Today's spending:
+            </Typography>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
+          <Paper className={classes.paper}>*£X.00*</Paper>
         </Grid>
       </Grid>
     </div>
   );
 }
+
+export { default as Spending } from "./Spending";
