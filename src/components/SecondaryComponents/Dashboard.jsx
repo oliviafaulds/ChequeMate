6688e8597699d1c0171bec94bd754f53bb9807cf
hayleyesthertaylor <83606121+hayleyesthertaylor@users.noreply.github.com>
2021-05-04 17:13:02 +0100

    Update Dashboard.jsx

--- src/components/SecondaryComponents/Dashboard.jsx
+++ src/components/SecondaryComponents/Dashboard.jsx
@@ -1,15 +1,59 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function Dashboard() {
-    return (
-        
-        <Typography variant="h1" component="h2" gutterBottom>
-          Dashboard Page
-        </Typography>   
+  const classes = useStyles();
 
-    );
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Typography
+            align="left"
+            variant="h1"
+            component="h2"
+            gutterbottom>
+            Welcome back,
+          </Typography>
+        </Grid>
+        <Grid item xs={5}>
+          <Typography
+            align="centre"
+            variant="h1"
+            component="h2"
+            gutterbottom>
+            *name*
+          </Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography
+            align="centre"
+            variant="h1"
+            component="h2"
+            gutterbottom>
+            !
+          </Typography>
+        </Grid>
+      </Grid>  
+    </div>
+  );
   }
   
   export { default as Dashboard } from "./Dashboard";
