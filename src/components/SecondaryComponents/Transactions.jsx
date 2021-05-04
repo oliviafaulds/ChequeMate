78f7ee0e3bd66d56fd8de5c5a212646ad3787ab0
JayClaw101 <johanna@cehs.net>
2021-05-04 20:09:33 +0100

    Transaction Page Format Attempt

--- src/components/SecondaryComponents/Transactions.jsx
+++ src/components/SecondaryComponents/Transactions.jsx
@@ -3,6 +3,14 @@ import { firestore } from "../../firebase";
 import ReactDOM from 'react-dom'
 import { Link, withRouter } from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import AddIcon from '@material-ui/icons/Add';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
 
 export default class Transactions extends React.Component {
 
@@ -28,13 +36,45 @@ export default class Transactions extends React.Component {
     render(){
         return(
             <div className= "Transactions">
-                <h1>Recent Transactions</h1>
+                <div>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography align="center" variant="h1" component="h2" gutterBottom>
+                                Transactions
+                            </Typography> 
+                            <Box display="flex" alignItems="center" justifyContent="center" m={4}>
+                                <Button variant="contained" color="primary" component={Link} to={'/newtransaction'} startIcon={<AddIcon />}>
+                                    New Transaction
+                                </Button>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </div>
                 {
                     this.state.Transactions &&
                     this.state.Transactions.map (Transactions => {
                         return(
                             <div>
-                                <p>{Transactions.item} - £{Transactions.cost}</p>
+                                <GridList cellHeight={160} cols={1}>
+                                    <GridListTile key={Transactions.item}>
+                                        <Typography align="center" variant="button" component="h2" gutterBottom>
+                                            {Transactions.item}
+                                        </Typography> 
+                                        <Typography align="center" variant="body2" component="h2" gutterBottom>
+                                            £{Transactions.cost}
+                                        </Typography> 
+                                        <Box display="flex" alignItems="center" justifyContent="center">
+                                            <Button color="primary">
+                                                Edit
+                                            </Button>
+                                        </Box>
+                                        <Box display="flex" alignItems="center" justifyContent="center">
+                                            <Button color="secondary">
+                                                Delete
+                                            </Button>
+                                        </Box>
+                                    </GridListTile>
+                                </GridList>
                             </div>
                         )
                     })
