8d6ff2fcd4bb4f51071c7052a41bdb6b08aaf90a
hayleyesthertaylor <83606121+hayleyesthertaylor@users.noreply.github.com>
2021-05-04 23:34:38 +0100

    Update Messages.jsx

--- src/components/SecondaryComponents/Messages.jsx
+++ src/components/SecondaryComponents/Messages.jsx
@@ -1,8 +1,10 @@
+// Import react
 import React from 'react';
-import { useContext } from 'react';
+
+// Import firestore information from project
 import { firestore } from "../../firebase";
-import { Link, withRouter } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
+
+// Import material UI components and icon
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -10,8 +12,11 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { render } from '@testing-library/react';
 
+//--------------------------------------------------------------------------------
+
+
+// Create and export the page to show Messages
 export default class Messages extends React.Component {
 
     state = { 
